4d2ef7a190161cc5e0113b22b81bcfdba1a6515b
IanGallacher <gallachergithub@outlook.com>
2017-10-10 21:34:02 -0700

    Fixes issue where reapers get stuck trying to jump down cliffs by lowering the APM. Refactor Micro.cpp to use the suffix tag whenever refereing to a tag. Optimized performance by only searching for the unit pointer once per function.

--- src/micro/Micro.cpp
+++ src/micro/Micro.cpp
@@ -5,7 +5,7 @@
 
 const float dotRadius = 0.1f;
 
-void Micro::SmartAttackUnit(const sc2::Tag & attacker, const sc2::Tag & target, ByunJRBot & bot)
+void Micro::SmartAttackUnit(const sc2::Tag & attackerTag, const sc2::Tag & targetTag, ByunJRBot & bot)
 {
 	//UAB_ASSERT(attacker, "SmartAttackUnit: Attacker not valid");
 	//UAB_ASSERT(target, "SmartAttackUnit: Target not valid");
@@ -40,19 +40,33 @@ void Micro::SmartAttackUnit(const sc2::Tag & attacker, const sc2::Tag & target, 
 	//	BWAPI::Broodwar->drawCircleMap(target->getPosition(), dotRadius, BWAPI::Colors::Red, true);
 	//	BWAPI::Broodwar->drawLineMap(attacker->getPosition(), target->getPosition(), BWAPI::Colors::Red);
 	//}
-    bot.Actions()->UnitCommand(bot.GetUnit(attacker), sc2::ABILITY_ID::ATTACK_ATTACK, bot.GetUnit(target));
+
+    // Prevent sending duplicate commands to give an accurate APM measurement in replays
+    bool sentCommandAlready = false;
+    const sc2::Unit * attacker = bot.GetUnit(attackerTag);
+    const sc2::Unit * target = bot.GetUnit(targetTag);
+    for (sc2::UnitOrder theOrder : attacker->orders)
+    {
+        if (theOrder.ability_id == sc2::ABILITY_ID::MOVE && theOrder.target_unit_tag == targetTag)
+        {
+            sentCommandAlready = true;
+        }
+    }
+    if (sentCommandAlready == false)
+        bot.Actions()->UnitCommand(attacker, sc2::ABILITY_ID::ATTACK_ATTACK, target);
 }
 
-void Micro::SmartAttackMove(const sc2::Tag & attacker, const sc2::Point2D & targetPosition, ByunJRBot & bot)
+void Micro::SmartAttackMove(const sc2::Tag & attackerTag, const sc2::Point2D & targetPosition, ByunJRBot & bot)
 {
-    bot.Actions()->UnitCommand(bot.GetUnit(attacker), sc2::ABILITY_ID::ATTACK_ATTACK, targetPosition);
+    bot.Actions()->UnitCommand(bot.GetUnit(attackerTag), sc2::ABILITY_ID::ATTACK_ATTACK, targetPosition);
 }
 
-void Micro::SmartMove(const sc2::Tag & unitToMove, const sc2::Point2D & targetPosition, ByunJRBot & bot)
+void Micro::SmartMove(const sc2::Tag & unitTag, const sc2::Point2D & targetPosition, ByunJRBot & bot)
 {
     // Prevent sending duplicate commands to give an accurate APM measurement in replays
     bool sentCommandAlready = false;
-    for (sc2::UnitOrder theOrder : bot.GetUnit(unitToMove)->orders)
+    const sc2::Unit * unitToMove = bot.GetUnit(unitTag);
+    for (sc2::UnitOrder theOrder : unitToMove->orders)
     {
         if (theOrder.ability_id == sc2::ABILITY_ID::MOVE && theOrder.target_pos == targetPosition)
         {
@@ -60,20 +74,20 @@ void Micro::SmartMove(const sc2::Tag & unitToMove, const sc2::Point2D & targetPo
         }
     }
     if (sentCommandAlready == false)
-        bot.Actions()->UnitCommand(bot.GetUnit(unitToMove), sc2::ABILITY_ID::MOVE, targetPosition);
+        bot.Actions()->UnitCommand(unitToMove, sc2::ABILITY_ID::MOVE, targetPosition);
 }
 
-void Micro::SmartRightClick(const sc2::Tag & unit, const sc2::Tag & target, ByunJRBot & bot)
+void Micro::SmartRightClick(const sc2::Tag & unitTag, const sc2::Tag & targetTag, ByunJRBot & bot)
 {
-    bot.Actions()->UnitCommand(bot.GetUnit(unit), sc2::ABILITY_ID::SMART, bot.GetUnit(target));
+    bot.Actions()->UnitCommand(bot.GetUnit(unitTag), sc2::ABILITY_ID::SMART, bot.GetUnit(targetTag));
 }
 
-void Micro::SmartRepair(const sc2::Tag & unit, const sc2::Tag & target, ByunJRBot & bot)
+void Micro::SmartRepair(const sc2::Tag & unitTag, const sc2::Tag & targetTag, ByunJRBot & bot)
 {
-    bot.Actions()->UnitCommand(bot.GetUnit(unit), sc2::ABILITY_ID::SMART, bot.GetUnit(target));
+    bot.Actions()->UnitCommand(bot.GetUnit(unitTag), sc2::ABILITY_ID::SMART, bot.GetUnit(targetTag));
 }
 
-void Micro::SmartKiteTarget(const sc2::Tag & rangedUnit, const sc2::Tag & target, ByunJRBot & bot)
+void Micro::SmartKiteTarget(const sc2::Tag & rangedUnitTag, const sc2::Tag & targetTag, ByunJRBot & bot)
 {
     //UAB_ASSERT(rangedUnit, "SmartKiteTarget: Unit not valid");
     //UAB_ASSERT(target, "SmartKiteTarget: Target not valid");
@@ -82,8 +96,9 @@ void Micro::SmartKiteTarget(const sc2::Tag & rangedUnit, const sc2::Tag & target
     //{
     //	return;
     //}
-
-    const float range = Util::GetAttackRange(bot.GetUnit(rangedUnit)->unit_type, bot);
+    const sc2::Unit * rangedUnit = bot.GetUnit(rangedUnitTag);
+    const sc2::Unit * target = bot.GetUnit(targetTag);
+    const float range = Util::GetAttackRange(rangedUnit->unit_type, bot);
 
     //// determine whether the target can be kited
     //bool kiteLonger = Config::Micro::KiteLongerRangedUnits.find(rangedUnit->getType()) != Config::Micro::KiteLongerRangedUnits.end();
@@ -95,8 +110,8 @@ void Micro::SmartKiteTarget(const sc2::Tag & rangedUnit, const sc2::Tag & target
     //}
 
     bool kite(true);
-    const double dist(bot.Map().getGroundDistance(bot.GetUnit(rangedUnit)->pos, bot.GetUnit(target)->pos));
-    const double speed(bot.Observation()->GetUnitTypeData()[bot.GetUnit(rangedUnit)->unit_type].movement_speed);
+    const double dist(bot.Map().getGroundDistance(rangedUnit->pos, target->pos));
+    const double speed(bot.Observation()->GetUnitTypeData()[rangedUnit->unit_type].movement_speed);
 
 
     // if the unit can't attack back don't kite
@@ -107,55 +122,55 @@ void Micro::SmartKiteTarget(const sc2::Tag & rangedUnit, const sc2::Tag & target
 
     const double timeToEnter = (dist - range) / speed;
     // If we start moving back to attack, will our weapon be off cooldown?
-    if ((timeToEnter >= bot.GetUnit(rangedUnit)->weapon_cooldown))
+    if ((timeToEnter >= rangedUnit->weapon_cooldown))
     {
         kite = false;
     }
 
-    if (Util::IsBuilding(bot.GetUnit(target)->unit_type))
+    if (Util::IsBuilding(target->unit_type))
     {
         kite = false;
     }
 
     sc2::Point2D fleePosition;
-    if (bot.GetUnit(rangedUnit)->health < Util::EnemyDPSInRange(bot.GetUnit(rangedUnit)->pos, bot) + 5.0)
+    if (rangedUnit->health < Util::EnemyDPSInRange(rangedUnit->pos, bot) + 5.0)
     {
-        //std::cout << Util::EnemyDPSInRange(bot.GetUnit(rangedUnit)->pos, bot) << std::endl;
+        //std::cout << Util::EnemyDPSInRange(rangedUnit->pos, bot) << std::endl;
         kite = true;
         fleePosition = bot.Bases().getPlayerStartingBaseLocation(PlayerArrayIndex::Self)->getPosition();
     }
     else
     {
         // kite if we are not close to death.
-        fleePosition = bot.GetUnit(rangedUnit)->pos - bot.GetUnit(target)->pos + bot.GetUnit(rangedUnit)->pos;
+        fleePosition = rangedUnit->pos - target->pos + rangedUnit->pos;
     }
 
     //// if we can't shoot, run away
     if (kite)
     {
-        //fleePosition = bot.GetUnit(rangedUnit)->pos - bot.GetUnit(target)->pos + bot.GetUnit(rangedUnit)->pos;
-        bot.Map().drawLine(bot.GetUnit(rangedUnit)->pos, fleePosition);
-        Micro::SmartMove(rangedUnit, fleePosition, bot);
+        //fleePosition = rangedUnit->pos - target->pos + rangedUnit->pos;
+        bot.Map().drawLine(rangedUnit->pos, fleePosition);
+        Micro::SmartMove(rangedUnit->tag, fleePosition, bot);
     }
     //// otherwise shoot
     else
     {
         //bot.Actions()->UnitCommand(rangedUnit, sc2::ABILITY_ID::EFFECT_KD8CHARGE, target);
-        SmartAttackUnit(rangedUnit, target, bot);
+        SmartAttackUnit(rangedUnit->tag, target->tag, bot);
     }
 }
 
-void Micro::SmartBuild(const sc2::Tag & builder, const sc2::UnitTypeID & buildingType, sc2::Point2D pos, ByunJRBot & bot)
+void Micro::SmartBuild(const sc2::Tag & builderTag, const sc2::UnitTypeID & buildingType, const sc2::Point2D pos, ByunJRBot & bot)
 {
-    bot.Actions()->UnitCommand(bot.GetUnit(builder), Util::UnitTypeIDToAbilityID(buildingType), pos);
+    bot.Actions()->UnitCommand(bot.GetUnit(builderTag), Util::UnitTypeIDToAbilityID(buildingType), pos);
 }
 
-void Micro::SmartBuildTag(const sc2::Tag & builder, const sc2::UnitTypeID & buildingType, sc2::Tag targetTag, ByunJRBot & bot)
+void Micro::SmartBuildTag(const sc2::Tag & builderTag, const sc2::UnitTypeID & buildingType, const sc2::Tag targetTag, ByunJRBot & bot)
 {
-    bot.Actions()->UnitCommand(bot.GetUnit(builder), Util::UnitTypeIDToAbilityID(buildingType), bot.GetUnit(targetTag));
+    bot.Actions()->UnitCommand(bot.GetUnit(builderTag), Util::UnitTypeIDToAbilityID(buildingType), bot.GetUnit(targetTag));
 }
 
-void Micro::SmartTrain(const sc2::Tag & builder, const sc2::UnitTypeID & buildingType, ByunJRBot & bot)
+void Micro::SmartTrain(const sc2::Tag & builderTag, const sc2::UnitTypeID & buildingType, ByunJRBot & bot)
 {
-    bot.Actions()->UnitCommand(bot.GetUnit(builder), Util::UnitTypeIDToAbilityID(buildingType));
+    bot.Actions()->UnitCommand(bot.GetUnit(builderTag), Util::UnitTypeIDToAbilityID(buildingType));
 }
