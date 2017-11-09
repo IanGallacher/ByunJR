53168c8dc1958586e5be15bb68b31a8cbb3fac8a
IanGallacher <gallachergithub@outlook.com>
2017-11-09 11:31:35 -0800

    Remove unneeded code.

--- src/information/UnitInfoManager.cpp
+++ src/information/UnitInfoManager.cpp
@@ -7,11 +7,7 @@
 UnitInfoManager::UnitInfoManager(sc2::Agent & bot)
     : bot_(bot)
 {
-    // Prevent null reference exceptions down the line. 
-    unit_data_[sc2::Unit::Alliance::Self] = UnitData();
-    unit_data_[sc2::Unit::Alliance::Ally] = UnitData();
-    unit_data_[sc2::Unit::Alliance::Neutral] = UnitData();
-    unit_data_[sc2::Unit::Alliance::Enemy] = UnitData();
+
 }
 
 void UnitInfoManager::OnStart()
