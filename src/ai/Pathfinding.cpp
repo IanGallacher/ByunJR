06f0ee4692bb2c9dbfa9bc4eb1defd47da5a9ec0
Ian Gallacher <igallacher@outlook.com>
2017-11-26 18:59:33 -0800

    Pathfinding update

--- src/ai/Pathfinding.cpp
+++ src/ai/Pathfinding.cpp
@@ -1,184 +1,248 @@
 #include <map>
 
 #include "ByunJRBot.h"
-#include "TechLab/util/Timer.hpp"
 #include "TechLab/util/Util.h"
 
 #include "ai/Pathfinding.h"
 #include "micro/Micro.h"
 
-inline std::pair<sc2::Point2DI, int> Pathfinding::BestPotentialPair(const std::pair<sc2::Point2DI, int> fallback_pair) const
+void Pathfinding::AddCandidatePoint(const CandidatePoint candidate_point)
 {
-    int smallest_distance = std::numeric_limits<int>::max();
-    std::pair<sc2::Point2DI, int> optimal_pair = fallback_pair;
-    for (const auto & distance_pair : distance_map_)
+	const auto p = candidate_point.nominated_point;
+
+	if (visited_.find(p) != visited_.end()) return;
+	if (p.x < 0) return;
+	if (p.x > map_width_ - 1) return;
+	if (p.y < 0) return;
+    if (p.y > map_height_ - 1) return;
+
+	visitable_points_.insert(candidate_point);
+}
+
+// Look through the visitable_points_ and find the best one to visit. 
+inline CandidatePoint Pathfinding::BestPotentialPoint(const CandidatePoint fallback_point) const
+{
+    CandidatePoint optimal_point = fallback_point;
+    for (const auto & point : visitable_points_)
     {
-        const sc2::Point2DI point = distance_pair.first;
-        const int dist = distance_pair.second;
+		// By definition, we should not be searching points that we have already visited. 
+		assert(visited_.find(point.nominated_point) == visited_.end());
+
+		const auto smallest_distance = optimal_point.weight;
+        const float dist = distance_map_.at(point.nominated_point);
         // If the distance to the node is 1, we have not yet found a path that leads to it.
-        if (dist < smallest_distance && dist != 1)
+        if (dist < smallest_distance || optimal_point == fallback_point)
         {
-            // Find the best pair that we have not yet visited. If we have already visited that point, don't visit it again. 
-            if (visited_.find(point) == visited_.end())
-            {
-                smallest_distance = dist;
-                optimal_pair = distance_pair;
-            }
+			optimal_point = point;
         }
     }
-    return optimal_pair;
+    return optimal_point;
 }
 
 
 // Will only test and update if the path has not yet been visited, garunteeing that the path will never loop on itself. 
-inline void Pathfinding::TestPointAndUpdateInformation(const int x, const int y, const int current_path_weight,
+inline void Pathfinding::TestPointAndUpdateInformation(const int x, const int y, const float current_path_weight,
                                                        const std::vector<std::vector<float>>& map_to_path,
                                                        const std::vector<sc2::Point2D>& current_shortest_path)
 {
-    Timer t;
-    t.Start();
     // The second value of the pair is the current weight of the node we are testing. 
-    const int new_weight = current_path_weight + static_cast<int>(map_to_path[y][x]);
+    const float new_weight = current_path_weight + map_to_path[y][x];
     // If we have visited the node we are about to test, the path we are considering taking loops back on itself. 
     // Test only the paths that do not loop back on themselves. 
     const sc2::Point2DI point{ x,y };
-    if (visited_.find(point) == visited_.end())
+
+    float & current_tentative_distance_at_point = distance_map_.at(sc2::Point2DI{x, y});
+    // If the distance to the node is 1, we have not yet found a path that leads to it. 
+    // Did we discover a more optimal path (or a path for the first time?)
+    // If we did, update the stored optimal path data.
+    if (new_weight < current_tentative_distance_at_point || current_tentative_distance_at_point == 1)
     {
-        int & current_tentative_distance_at_point = distance_map_.at(sc2::Point2DI{x, y});
-        // If the distance to the node is 1, we have not yet found a path that leads to it. 
-        // Did we discover a more optimal path (or a path for the first time?)
-        // If we did, update the stored optimal path data.
-        if (new_weight < current_tentative_distance_at_point || current_tentative_distance_at_point == 1)
-        {
-            current_tentative_distance_at_point = new_weight;
-            shortest_path_to_vector_[point] = current_shortest_path;
-            shortest_path_to_vector_[point].push_back(Util::ToPoint2D(point));
-        }
-    }/*
-    double ms = t.GetElapsedTimeInMilliSec();
-    printf("TestPointAndUpdateInformation %lf ms\n", ms);*/
+        current_tentative_distance_at_point = new_weight;
+        shortest_path_to_vector_[point] = current_shortest_path;
+        shortest_path_to_vector_[point].push_back(Util::ToPoint2D(point));
+    }
 }
 
 void Pathfinding::DjikstraInit(const std::vector<std::vector<float>>& map_to_path)
 {// Setup the values in the distance map.
 	visited_.clear();
     distance_map_.clear();
+	map_width_ = map_to_path[0].size();
+	map_height_ = map_to_path.size();
+
     for (int y = 0; y < map_to_path.size(); ++y)
     {
         for (int x = 0; x < map_to_path[y].size(); ++x)
         {
-            distance_map_.insert(std::make_pair(sc2::Point2DI{x, y}, 1));
+            distance_map_.insert(std::make_pair(sc2::Point2DI{x, y}, map_to_path[y][x]));
 
             shortest_path_to_vector_[sc2::Point2DI{x, y}] = std::vector<sc2::Point2D>();
         }
     }
 }
 
-std::vector<sc2::Point2D> Pathfinding::Djikstra(const sc2::Point2DI start_point,
-    const sc2::Point2DI end_point,
-    const std::vector<std::vector<float>>& map_to_path)
-{
-    DjikstraInit(map_to_path);
+// Djikstra will require a quick glance over. We no longer iterate through all the points on the map, but instead
+// all the points that have been recomended to visit. 
+//std::vector<sc2::Point2D> Pathfinding::Djikstra(const sc2::Point2DI start_point,
+//    const sc2::Point2DI end_point,
+//    const std::vector<std::vector<float>>& map_to_path)
+//{
+//    DjikstraInit(map_to_path);
+//
+//    // If we have not yet found the shortest path between the two points, keep searching for a new path. 
+//    while (true)
+//    {
+//        std::pair<sc2::Point2DI, int> p = BestPotentialPair(std::pair<sc2::Point2DI, int> { start_point, 2 });
+//        // Hard copy the current point into the visited set, and test nearby nodes if it has not yet been visited. 
+//        // visited.set.insert( ... ).second is set to true if we have not yet visited the element. =
+//        if (visited_.insert(p.first).second)
+//        {
+//            const int best_potential_x = p.first.x;
+//            const int best_potential_y = p.first.y;
+//            const int weight = p.second;
+//            // We can't compare best_potential_x to something that is off the map. 
+//            if (best_potential_x > 0)
+//            {
+//                TestPointAndUpdateInformation(best_potential_x - 1, best_potential_y, weight, map_to_path, shortest_path_to_vector_.at(p.first));
+//                const sc2::Point2DI current_point(best_potential_x - 1, best_potential_y);
+//                if (current_point == end_point)
+//                    break;
+//            }
+//            if (best_potential_x < map_to_path[0].size() - 1)
+//            {
+//                TestPointAndUpdateInformation(best_potential_x + 1, best_potential_y, weight, map_to_path, shortest_path_to_vector_.at(p.first));
+//                const sc2::Point2DI current_point(best_potential_x + 1, best_potential_y);
+//                if (current_point == end_point)
+//                    break;
+//            }
+//            if (best_potential_y > 0)
+//            {
+//                TestPointAndUpdateInformation(best_potential_x, best_potential_y - 1, weight, map_to_path, shortest_path_to_vector_.at(p.first));
+//                const sc2::Point2DI current_point(best_potential_x, best_potential_y - 1);
+//                if (current_point == end_point)
+//                    break;
+//            }
+//            if (best_potential_y < map_to_path.size() - 1)
+//            {
+//                TestPointAndUpdateInformation(best_potential_x, best_potential_y + 1, weight, map_to_path, shortest_path_to_vector_.at(p.first));
+//                const sc2::Point2DI current_point(best_potential_x, best_potential_y + 1);
+//                if (current_point == end_point)
+//                    break;
+//            }
+//        }
+//    }
+//    return shortest_path_to_vector_.at(end_point);
+//}
 
-    // If we have not yet found the shortest path between the two points, keep searching for a new path. 
-    while (true)
-    {
-        std::pair<sc2::Point2DI, int> p = BestPotentialPair(std::pair<sc2::Point2DI, int> { start_point, 2 });
-        // Hard copy the current point into the visited set, and test nearby nodes if it has not yet been visited. 
-        // visited.set.insert( ... ).second is set to true if we have not yet visited the element. =
-        if (visited_.insert(p.first).second)
-        {
-            const int best_potential_x = p.first.x;
-            const int best_potential_y = p.first.y;
-            const int weight = p.second;
-            // We can't compare best_potential_x to something that is off the map. 
-            if (best_potential_x > 0)
-            {
-                TestPointAndUpdateInformation(best_potential_x - 1, best_potential_y, weight, map_to_path, shortest_path_to_vector_.at(p.first));
-                const sc2::Point2DI current_point(best_potential_x - 1, best_potential_y);
-                if (current_point == end_point)
-                    break;
-            }
-            if (best_potential_x < map_to_path[0].size() - 1)
-            {
-                TestPointAndUpdateInformation(best_potential_x + 1, best_potential_y, weight, map_to_path, shortest_path_to_vector_.at(p.first));
-                const sc2::Point2DI current_point(best_potential_x + 1, best_potential_y);
-                if (current_point == end_point)
-                    break;
-            }
-            if (best_potential_y > 0)
-            {
-                TestPointAndUpdateInformation(best_potential_x, best_potential_y - 1, weight, map_to_path, shortest_path_to_vector_.at(p.first));
-                const sc2::Point2DI current_point(best_potential_x, best_potential_y - 1);
-                if (current_point == end_point)
-                    break;
-            }
-            if (best_potential_y < map_to_path.size() - 1)
-            {
-                TestPointAndUpdateInformation(best_potential_x, best_potential_y + 1, weight, map_to_path, shortest_path_to_vector_.at(p.first));
-                const sc2::Point2DI current_point(best_potential_x, best_potential_y + 1);
-                if (current_point == end_point)
-                    break;
-            }
-        }
-    }
-    return shortest_path_to_vector_.at(end_point);
-}
+// Will also require a glance over. 
+// Find the optimal path, searching no farther than max_run_dist. 
+// Uses a modified Djikstra algorithm. 
+//std::vector<sc2::Point2D> Pathfinding::DjikstraLimit(const sc2::Point2DI start_point,
+//    const int max_run_dist,
+//    const std::vector<std::vector<float>>& map_to_path)
+//{
+//    // Setup the values in the distance map.
+//    DjikstraInit(map_to_path);
+//
+//    sc2::Point2DI current_point;
+//
+//    // If we have not yet found the optimal path between the two points, keep searching for a new path. 
+//    while (true)
+//    {
+//        std::pair<sc2::Point2DI, int> p = BestPotentialPair(std::pair<sc2::Point2DI, int> { start_point, 2 });
+//        // Hard copy the current point into the visited set, and test nearby nodes if it has not yet been visited. 
+//        // visited.set.insert( ... ).second is set to true if we have not yet visited the element.
+//        if (visited_.insert(p.first).second)
+//        {
+//            const int best_potential_x = p.first.x;
+//            const int best_potential_y = p.first.y;
+//            const int weight = p.second;
+//            // We can't compare best_potential_x to something that is off the map. 
+//            if (best_potential_x > 0)
+//            {
+//                TestPointAndUpdateInformation(best_potential_x - 1, best_potential_y, weight, map_to_path, shortest_path_to_vector_.at(p.first));
+//                current_point = sc2::Point2DI{best_potential_x - 1, best_potential_y};
+//                if (shortest_path_to_vector_.at(current_point).size() >= max_run_dist)
+//                    break;
+//            }
+//            if (best_potential_x < map_to_path[0].size() - 1)
+//            {
+//                TestPointAndUpdateInformation(best_potential_x + 1, best_potential_y, weight, map_to_path, shortest_path_to_vector_.at(p.first));
+//                current_point = sc2::Point2DI{best_potential_x + 1, best_potential_y};
+//                if (shortest_path_to_vector_.at(current_point).size() >= max_run_dist)
+//                    break;
+//            }
+//            if (best_potential_y > 0)
+//            {
+//                TestPointAndUpdateInformation(best_potential_x, best_potential_y - 1, weight, map_to_path, shortest_path_to_vector_.at(p.first));
+//                current_point = sc2::Point2DI{best_potential_x, best_potential_y - 1};
+//                if (shortest_path_to_vector_.at(current_point).size() >= max_run_dist)
+//                    break;
+//            }
+//            if (best_potential_y < map_to_path.size() - 1)
+//            {
+//                TestPointAndUpdateInformation(best_potential_x, best_potential_y + 1, weight, map_to_path, shortest_path_to_vector_.at(p.first));
+//                current_point = sc2::Point2DI{best_potential_x, best_potential_y + 1};
+//                if (shortest_path_to_vector_.at(current_point).size() >= max_run_dist)
+//                    break;
+//            }
+//        }
+//    }
+//    return shortest_path_to_vector_.at(current_point);
+//}
 
 // Find the optimal path, searching no farther than max_run_dist. 
 // Uses a modified Djikstra algorithm. 
-std::vector<sc2::Point2D> Pathfinding::DjikstraLimit(const sc2::Point2DI start_point,
-    const int max_run_dist,
-    const std::vector<std::vector<float>>& map_to_path)
+std::vector<sc2::Point2D> Pathfinding::OptimalPath(const sc2::Point2DI start_point,
+	const int max_run_dist,
+	const std::vector<std::vector<float>>& map_to_path)
 {
-    // Setup the values in the distance map.
-    DjikstraInit(map_to_path);
+	// Setup the values in the distance map.
+	DjikstraInit(map_to_path);
 
-    sc2::Point2DI current_point;
+	sc2::Point2DI current_point;
 
-    // If we have not yet found the optimal path between the two points, keep searching for a new path. 
-    while (true)
-    {
-        std::pair<sc2::Point2DI, int> p = BestPotentialPair(std::pair<sc2::Point2DI, int> { start_point, 2 });
-        // Hard copy the current point into the visited set, and test nearby nodes if it has not yet been visited. 
-        // visited.set.insert( ... ).second is set to true if we have not yet visited the element.
-        if (visited_.insert(p.first).second)
-        {
-            const int best_potential_x = p.first.x;
-            const int best_potential_y = p.first.y;
-            const int weight = p.second;
-            // We can't compare best_potential_x to something that is off the map. 
-            if (best_potential_x > 0)
-            {
-                TestPointAndUpdateInformation(best_potential_x - 1, best_potential_y, weight, map_to_path, shortest_path_to_vector_.at(p.first));
-                current_point = sc2::Point2DI{best_potential_x - 1, best_potential_y};
-                if (shortest_path_to_vector_.at(current_point).size() >= max_run_dist)
-                    break;
-            }
-            if (best_potential_x < map_to_path[0].size() - 1)
-            {
-                TestPointAndUpdateInformation(best_potential_x + 1, best_potential_y, weight, map_to_path, shortest_path_to_vector_.at(p.first));
-                current_point = sc2::Point2DI{best_potential_x + 1, best_potential_y};
-                if (shortest_path_to_vector_.at(current_point).size() >= max_run_dist)
-                    break;
-            }
-            if (best_potential_y > 0)
-            {
-                TestPointAndUpdateInformation(best_potential_x, best_potential_y - 1, weight, map_to_path, shortest_path_to_vector_.at(p.first));
-                current_point = sc2::Point2DI{best_potential_x, best_potential_y - 1};
-                if (shortest_path_to_vector_.at(current_point).size() >= max_run_dist)
-                    break;
-            }
-            if (best_potential_y < map_to_path.size() - 1)
-            {
-                TestPointAndUpdateInformation(best_potential_x, best_potential_y + 1, weight, map_to_path, shortest_path_to_vector_.at(p.first));
-                current_point = sc2::Point2DI{best_potential_x, best_potential_y + 1};
-                if (shortest_path_to_vector_.at(current_point).size() >= max_run_dist)
-                    break;
-            }
-        }
-    }
-    return shortest_path_to_vector_.at(current_point);
+	// If we have not yet found the optimal path between the two points, keep searching for a new path. 
+	while (true)
+	{
+		CandidatePoint p = BestPotentialPoint(CandidatePoint { start_point, start_point, 2 });
+
+		const int best_potential_x = p.nominated_point.x;
+		const int best_potential_y = p.nominated_point.y;
+		AddCandidatePoint(CandidatePoint
+			{ 
+			  sc2::Point2DI(best_potential_x - 1, best_potential_y),
+			  p.nominated_point,
+			  1});
+		AddCandidatePoint(CandidatePoint
+			{
+			  sc2::Point2DI(best_potential_x + 1, best_potential_y),
+			  p.nominated_point,
+			  1});
+		AddCandidatePoint(CandidatePoint
+			{ 
+			  sc2::Point2DI(best_potential_x, best_potential_y - 1),
+			  p.nominated_point,
+			  1});
+		AddCandidatePoint(CandidatePoint
+			{ 
+			  sc2::Point2DI(best_potential_x, best_potential_y + 1),
+			  p.nominated_point,
+			  1});
+
+		visited_.insert(p.nominated_point);
+
+		// We can't compare best_potential_x to something that is off the map. 
+		TestPointAndUpdateInformation(best_potential_x, best_potential_y, p.weight, map_to_path, 
+			shortest_path_to_vector_.at(p.origin_point));
+		current_point = sc2::Point2DI{ best_potential_x, best_potential_y };
+		int j = shortest_path_to_vector_.at(current_point).size();
+		if (shortest_path_to_vector_.at(current_point).size() >= max_run_dist)
+			break;
+
+		visitable_points_.erase(p);
+	}
+	return shortest_path_to_vector_.at(current_point);
 }
 
 bool Pathfinding::TestDjikstra()
@@ -219,11 +283,11 @@ bool Pathfinding::TestDjikstra()
         sc2::Point2D{ 4,4 }
     };
 
-    std::vector<sc2::Point2D> result = Djikstra(start_point, end_point, map_to_path);
+ /*   std::vector<sc2::Point2D> result = Djikstra(start_point, end_point, map_to_path);
     if (result[0] == expected_result[0])
     {
         return true;
-    }
+    }*/
     return false;
 }
 
@@ -237,19 +301,19 @@ void Pathfinding::SmartPathfind(const sc2::Unit* unit, const sc2::Point2D & targ
         Micro::SmartMove(unit, target_position, bot);
         return;
     }
-
+/*
     Pathfinding p;
     std::vector<sc2::Point2D> move_path = p.Djikstra(Util::ToPoint2DI(unit->pos),
         Util::ToPoint2DI(target_position),
         bot.Info().GetDPSMap());
-    Micro::SmartMove(unit, move_path[0], bot);
+    Micro::SmartMove(unit, move_path[0], bot);*/
 }
 
 
 void Pathfinding::SmartRunAway(const sc2::Unit* unit, const int run_distance, ByunJRBot & bot)
 {
     Pathfinding p;
-    std::vector<sc2::Point2D> move_path = p.DjikstraLimit(Util::ToPoint2DI(unit->pos),
+    std::vector<sc2::Point2D> move_path = p.OptimalPath(Util::ToPoint2DI(unit->pos),
         run_distance,
         bot.Info().GetDPSMap());
     //SmartMove(unit, move_path[0], bot, false);
--- src/ai/Pathfinding.h
+++ src/ai/Pathfinding.h
@@ -5,6 +5,19 @@
 
 #include <sc2api/sc2_api.h>
 
+
+struct CandidatePoint
+{
+	// origin_point is the tip of the path that leads to nominated_point
+	sc2::Point2DI nominated_point;
+	sc2::Point2DI origin_point;
+	float weight; // Weight will normally be the same as distance for most pathfinding algorithms, but does not have to be.
+
+	bool operator==(const CandidatePoint& a) const
+	{
+		return nominated_point.x == a.nominated_point.x && nominated_point.y == a.nominated_point.y;
+	}
+};
 namespace std
 {
     template <> struct hash<sc2::Point2DI>
@@ -18,29 +31,48 @@ namespace std
             return k.x * 10000 + k.y;
         }
     };
+
+	template <> struct hash<CandidatePoint>
+	{
+		size_t operator()(const CandidatePoint& k) const
+		{
+			// This is probably a better hash function to use in the future. 
+			// return (53 + hash<int>(k.x)) * 53 + hash<int>(k.y);
+
+			// For now, this hash function is good enough for our uses. 
+			return k.nominated_point.x * 10000 + k.nominated_point.y;
+		}
+	};
 }
 
 class Pathfinding {
-    std::pair<sc2::Point2DI, int> BestPotentialPair(std::pair<sc2::Point2DI, int> fallback_pair) const;
-    void TestPointAndUpdateInformation(const int x, const int y, const int current_path_weight,
+    CandidatePoint BestPotentialPoint(CandidatePoint fallback_pair) const;
+    void TestPointAndUpdateInformation(const int x, const int y, const float current_path_weight,
                                        const std::vector<std::vector<float>>& map_to_path,
                                        const std::vector<sc2::Point2D>& current_shortest_path);
 
+	int map_width_;
+	int map_height_;
+
     // We can't use a set because Point2DI does not have a comparison function, and sets must be "ordered" somehow.
     std::unordered_set<sc2::Point2DI> visited_;
+	std::unordered_set<CandidatePoint> visitable_points_;
 
     // The distance of the optimal route to that path.
-    std::unordered_map<sc2::Point2DI, int> distance_map_;
+	// Initially setup with every point on the map, with a value of one. 
+    std::unordered_map<sc2::Point2DI, float> distance_map_;
     std::unordered_map<sc2::Point2DI, std::vector<sc2::Point2D>> shortest_path_to_vector_;
 
     void DjikstraInit(const std::vector<std::vector<float>>& map_to_path);
+	void AddCandidatePoint(const CandidatePoint c);
 public:
     // Returns the optimal path. 
-    std::vector<sc2::Point2D> Djikstra(const sc2::Point2DI start_point, const sc2::Point2DI end_point,
-                                       const std::vector<std::vector<float>>& map_to_path);
-    std::vector<sc2::Point2D> DjikstraLimit(const sc2::Point2DI start_point, const int max_run_dist,
-                                            const std::vector<std::vector<float>>& map_to_path);
-    bool TestDjikstra();
+    //std::vector<sc2::Point2D> Djikstra(const sc2::Point2DI start_point, const sc2::Point2DI end_point,
+    //                                   const std::vector<std::vector<float>>& map_to_path);
+    //std::vector<sc2::Point2D> DjikstraLimit(const sc2::Point2DI start_point, const int max_run_dist,
+    //                                        const std::vector<std::vector<float>>& map_to_path);
+	std::vector<sc2::Point2D> OptimalPath(const sc2::Point2DI start_point, const int max_run_dist, const std::vector<std::vector<float>>& map_to_path);
+	bool TestDjikstra();
     void SmartPathfind(const sc2::Unit * unit, const sc2::Point2D & target_position, ByunJRBot & bot);
     void SmartRunAway(const sc2::Unit * unit, const int run_distance, ByunJRBot & bot);
 };
